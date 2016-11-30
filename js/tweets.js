160722e1f81a8c8a6e1a45e003629e2bad7fef07
ilanFleitman <ilan.fleitman@gmail.com>
2016-11-30 16:54:58 +0200

    exrecise 3 - all most finish need testing and fix code

--- js/tweets.js
+++ js/tweets.js
@@ -1,17 +1,39 @@
-/**
- * Created by Jbt on 11/30/2016.
- */
-var tweets = [ {username: 'Bobo', text: "hello"}];
+var tweets = [
+    {username: 'James Bond', text: "Blablabla..."},
+    {username: 'James Bond', text: "I'm hungry"},
+    {username: 'Albert Einstein', text: "E = mc^2"},
+    {username: 'Bill Gates', text: "I think 64 bytes should be enough for everyone"},
+    {username: 'Frodo', text: "My Precious"}
+    ];
 
+var myUsername = 'ilan';
+
+function encodeHTML(text) {
+    return text.replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
+
+function createDiv(clazz) {
+    var div = document.createElement('div');
+    div.className = clazz;
+    return div;
+}
+
+function eraseElements(parantDiv) {
+    while (parantDiv.hasChildNodes())
+        parantDiv.removeChild(parantDiv.lastChild);
+
+}
 
 var publishTweet = function () {
     var tweetText = document.getElementById('text-area');
-    var tweetWithUsername = {username: 'ilan', text: tweetText.value};
+    var tweetWithUsername = {username: myUsername, text: encodeHTML(tweetText.value)};
     tweets.push(tweetWithUsername);
     tweetText.value = '';
-    var getTweetDiv = document.getElementById('tweets');
-    while (getTweetDiv.hasChildNodes())
-        getTweetDiv.removeChild(getTweetDiv.lastChild);
+    eraseElements(document.getElementById('tweets'));
     putTweets();
 }
 
@@ -20,24 +42,19 @@ function putTweets() {
     var getTweetDiv = document.getElementById('tweets');
 
     tweets.forEach(function (tweet) {
-        var div = document.createElement('div');
-        div.className = "row";
+        var div = createDiv("row");
 
         var img = document.createElement('img');
         img.className = "col-md-1 avatarpic";
         img.src = "../images/useravatar.png";
 
-        var innerDiv = document.createElement('div');
-        innerDiv.className = 'col-md-2';
+        var innerDiv = createDiv('col-md-5');
+        var userNameDiv = createDiv('row');
+        var userName = document.createElement('label');
+        userName.className = tweet.username === myUsername ? "" : "notMe";
+        userName.innerHTML = tweet.username + " says:";
 
-        var userNameDiv = document.createElement('div');
-        userNameDiv.className = 'row';
-
-        var userName = document.createElement('b');
-        userName.innerHTML = tweet.username;
-
-        var tweetText = document.createElement('div');
-        tweetText.className = 'row';
+        var tweetText = createDiv('row');
         tweetText.innerHTML = tweet.text;
 
         userNameDiv.appendChild(userName);
--- js/users.js
+++ js/users.js
@@ -1,3 +1,95 @@
-/**
- * Created by Jbt on 11/30/2016.
- */
+var users = [
+    {username: 'Marty McFly', follow: false},
+    {username: 'Janis Joplin', follow: false},
+    {username: 'Albert Einstein', follow: false},
+    {username: 'Genghis Khan', follow: false},
+    {username: 'Dracula', follow: false},
+    {username: 'Forest Gump', follow: false},
+    {username: 'Caligula', follow: false},
+    {username: 'Winnie the Pooh', follow: false},
+    {username: 'Obama', follow: false},
+    {username: 'Henry the 8th', follow: false},
+];
+
+function createDiv(clazz) {
+    var div = document.createElement('div');
+    div.className = clazz;
+    return div;
+}
+
+function createImg() {
+    var img = document.createElement('img');
+    img.className = "avatarpic";
+    img.src = "../images/useravatar.png";
+    return img;
+}
+
+function eraseElements(parantDiv) {
+    while (parantDiv.hasChildNodes())
+        parantDiv.removeChild(parantDiv.lastChild);
+
+}
+
+function putUsers(usersOfTwitter, textToLook, cols) {
+    var docfrag = document.createDocumentFragment();
+    var getUsersDiv = document.getElementById(textToLook);
+
+    usersOfTwitter.forEach(function (user) {
+        var div = createDiv("col-md-" + cols + "");
+        var img = createImg();
+        var innerRow = createDiv('row');
+        var centerDiv = createDiv('text-center col-md-12');
+        var thumbnail = createDiv('thumbnail');
+        var caption = createDiv('caption');
+
+        var followBtn = document.createElement('button');
+        followBtn.className = 'btn btn-primary';
+        followBtn.innerHTML = user.follow ? "unfollow" : "follow";
+        followBtn.addEventListener('click', function () {
+            user.follow = !user.follow;
+            eraseElements(document.getElementById('users'));
+            eraseElements(document.getElementById('followes'));
+            filterByName(document.getElementById('filterText').value);
+            putFollowes('followes', 12);
+        });
+
+        var space = document.createElement('p');
+
+        var userName = document.createElement('p');
+        userName.innerHTML = user.username;
+
+        caption.appendChild(followBtn);
+        caption.appendChild(space);
+        caption.appendChild(userName);
+        thumbnail.appendChild(img);
+        thumbnail.appendChild(caption);
+        centerDiv.appendChild(thumbnail);
+        innerRow.appendChild(centerDiv);
+        div.appendChild(innerRow);
+        docfrag.appendChild(div);
+    });
+
+    getUsersDiv.appendChild(docfrag);
+}
+
+function putFollowes(textToLook, cols) {
+    putUsers(users.filter(function (user) {
+        return user.follow;
+    }), textToLook, cols)
+}
+
+function filterByName(userName) {
+   eraseElements(document.getElementById('users'));
+    putUsers(users.filter(function (user) {
+        return user.username.toLocaleLowerCase().includes(userName.toLowerCase());
+    }), 'users', 2);
+}
+
+window.onload = function () {
+    var filterText = document.getElementById('filterText');
+    filterText.addEventListener('keyup', function () {
+        filterByName(filterText.value);
+    });
+    putUsers(users, 'users', 2);
+    putFollowes('followes', 12);
+};
